cea8afad5c69fb429215c0b3ad745414f211bf63
Cesar Carbajo <cesares10@gmail.com>
2019-08-03 20:07:18 +0200

    Basic player movement. Sprites extracted from spritesheet

---
+++ GMTKjam2019Entry/Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    #region Variables
+
+    public int ID;
+    public Player player;
+    public Enemy enemy;
+
+    #endregion
+
+    #region Properties
+
+    public float pSpeed;
+
+    #endregion
+
+    #region MonoBehaviour Functions
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    #endregion
+}
---
+++ GMTKjam2019Entry/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    #region Variables
+
+    public float angle = 0.0f;
+    public float speed = 0.05f;
+    public float rotSpeed = 1.5f;
+
+    public Bullet bullet;
+
+    #endregion
+
+    #region Constructor and Getters
+
+
+    #endregion
+
+    #region Methods
+
+    void Update()
+    {
+
+        //Movement
+        float horInput = Input.GetAxis("Horizontal");
+        float verInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horInput, verInput, 0.0f);
+        this.transform.position += direction * speed;
+
+        //Rotation
+        if (Input.GetKey(KeyCode.R))
+        {
+            this.transform.Rotate(new Vector3(0, 0, rotSpeed));
+        }
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            this.transform.Rotate(new Vector3(0, 0, -rotSpeed));
+        }
+
+        //Shooting
+    }
+
+    #endregion
+
+}
